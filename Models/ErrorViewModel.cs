2cc1536bdcdd3809c36aa1124cf75df8c97ac024
Fernando <ALUMNO@ULSLAB506>
2025-05-28 19:38:35 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T2_QUIROZ_MIGUEL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
